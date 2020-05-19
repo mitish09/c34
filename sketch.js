13cc265eb655965562507dcd92fba5c2803eba52
mitish09 <sundevelg@gmail.com>
2020-05-19 19:52:46 +0530

    Database Game

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
-var ball;
-
+var ball,database;
+var position;
 function setup(){
+    database = firebase.database();
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+    var ballPosition = database.ref('ball/position');
+    ballPosition.on("value",readPosition,showError);
 }
 
 function draw(){
@@ -24,6 +27,18 @@ function draw(){
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    database.ref('ball/position').set({
+        'x': position.x+x, 
+        'y': position.y+y,
+    })
+}
+
+function readPosition(data){
+    position = data.val()
+    ball.x = position.x;
+    ball.y = position.y;
+
+}
+function showError(){
+    console.log("errorInWritingInDatabase")
 }
